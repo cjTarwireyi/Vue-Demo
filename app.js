93f56687ddcc7ccb02045c0a0f8e5a0e1ff30678
cornelious <cornelioust@dipar.co.za>
2022-03-14 21:50:23 +0200

    added computed property

--- app.js
+++ app.js
@@ -3,12 +3,22 @@ const app = Vue.createApp({
 data() {
     return {
         title:'Vue Demo',
-        count:0
+        count:0,
+        books:[
+            {title: 'vue demo book', author:'cj Tar', isFav: true},
+            {title: 'Angular demo book', author:'cj Tar', isFav: false},
+            {title: 'React demo book', author:'cj Tar', isFav: true}
+        ]
     }
 },
 methods:{
     countValue(){
       this.count++
+    }
+},
+computed:{
+    filteredBooks(){
+        return this.books.filter(book => book.isFav);
     }
 }
 });
